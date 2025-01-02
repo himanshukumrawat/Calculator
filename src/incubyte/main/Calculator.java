2ad6226fe53127989391ac73c9d8aae95d3641ab
Himanshu Kumrawat <kumrawat.h@gmail.com>
2025-01-02 10:08:24 +0530

    Refactored to support max 2 numbers addition

--- src/incubyte/main/Calculator.java
+++ src/incubyte/main/Calculator.java
@@ -1,8 +1,20 @@
 package incubyte.main;
 
 public class Calculator {
-
     public int add(String numbers) {
-        return 0;
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        String[] numArray = numbers.split(",");
+        if (numArray.length > 2) {
+            throw new IllegalArgumentException("Only 2 numbers are supported");
+        }
+
+        int sum = 0;
+        for (String num : numArray) {
+            sum += Integer.parseInt(num.trim());
+        }
+        return sum;
     }
 }
--- src/incubyte/test/CalculatorTest.java
+++ src/incubyte/test/CalculatorTest.java
@@ -2,6 +2,9 @@ package incubyte.test;
 
 import incubyte.main.Calculator;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class CalculatorTest {
 
     Calculator calculator = new Calculator();
@@ -13,5 +16,34 @@ class CalculatorTest {
     void testAdd() {
         int res = calculator.add("");
         assert res == 0;
+    }
+
+    @Test
+    void testAddSingle() {
+        int res = calculator.add("1");
+        assert res == 1;
+    }
+
+    @Test
+    void testAddMultiple() {
+        int res = calculator.add("1,2");
+        assert res == 3;
+    }
+
+    @Test
+    void testAddMultipleWithTrailingComma() {
+        int res = calculator.add("1,");
+        assert res == 1;
+    }
+
+    @Test
+    void testAddMultipleWithWhiteSpace() {
+        int res = calculator.add("1, 2");
+        assert res == 3;
+    }
+
+    @Test
+    void testAddMultipleWith3Numbers() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.add("1,2,3"));
     }
 }
